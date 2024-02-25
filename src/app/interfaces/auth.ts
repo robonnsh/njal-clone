5e36a394b7d8e81b2e791b272e149d1ff193eeee
robo ninoshvili <nnshtofficial@gmail.com>
2024-02-26 01:48:28 +0400

    remove interface

--- src/app/interfaces/auth.ts
+++ src/app/interfaces/auth.ts
@@ -1,6 +1,6 @@
-export interface User {
-  id: string;
-  labelName: string;
-  email: string;
-  password: string;
-}
+// export interface User {
+//   id: string;
+//   labelName: string;
+//   email: string;
+//   password: string;
+// }
