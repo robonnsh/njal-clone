ab7a774f5960813410f197f837875e6877720178
robo ninoshvili <nnshtofficial@gmail.com>
2024-02-26 19:52:05 +0400

    change login component

--- src/app/shell/login/login.component.ts
+++ src/app/shell/login/login.component.ts
@@ -1,33 +1,80 @@
 import { Component } from '@angular/core';
-import { Validators, FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import { ToastrService } from 'ngx-toastr';
-
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  form!: FormGroup;
+  isLoggingIn = false;
+  isRecoveringPassword = false;
+  isRegistering = false;
+  errorMessage: string = '';
+  hide: boolean = true;
   showLoginComponents: boolean = true;
   isAsideNavOpen: boolean = false;
-  loginForm = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  });
-  get email() {
-    return this.loginForm.controls['email'];
-  }
-  get password() {
-    return this.loginForm.controls['password'];
-  }
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private toast: ToastrService
-  ) {}
+    private snackBar: MatSnackBar
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  login() {
+    this.isLoggingIn = true;
+
+    this.authService
+      .signIn({
+        email: this.form.value.email,
+        password: this.form.value.password,
+      })
+      .subscribe({
+        next: () => this.router.navigate(['home']),
+        error: (error) => {
+          this.isLoggingIn = false;
+          this.snackBar.open(error.message, 'OK', {
+            horizontalPosition: this.horizontalPosition,
+            verticalPosition: this.verticalPosition,
+            duration: 3000,
+          });
+        },
+      });
+  }
+
+  recoverPassword() {
+    this.isRecoveringPassword = true;
+
+    this.authService.recoverPassword(this.form.value.email).subscribe({
+      next: () => {
+        this.isRecoveringPassword = false;
+      },
+      error: (error) => {
+        this.isRecoveringPassword = false;
+      },
+    });
+  }
 
   onToggleAsideNav() {
     this.isAsideNavOpen = !this.isAsideNavOpen;
@@ -38,18 +85,4 @@ export class LoginComponent {
       this.isAsideNavOpen = false;
     }
   }
-
-  // loginUser() {
-  //   if (this.loginForm.valid) {
-  //     const { email, password } = this.loginForm.value;
-  //     this.authService.getUserByEmail(email as string).subscribe((res) => {
-  //       if (res.length > 0 && res[0].password === password) {
-  //         this.toast.success('Login successfully');
-  //         this.router.navigate(['/user-logged']);
-  //       } else {
-  //         this.toast.error('Invalid email or password');
-  //       }
-  //     });
-  //   }
-  // }
 }
