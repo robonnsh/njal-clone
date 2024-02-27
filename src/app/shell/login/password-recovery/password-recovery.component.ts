203cbf75a6940ba4d33bedc195df979067c1d346
robo ninoshvili <nnshtofficial@gmail.com>
2024-02-27 17:13:47 +0400

    add password recovery page

---
+++ src/app/shell/login/password-recovery/password-recovery.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-password-recovery',
+  templateUrl: './password-recovery.component.html',
+  styleUrl: './password-recovery.component.scss',
+})
+export class PasswordRecoveryComponent implements OnInit {
+  form!: FormGroup;
+
+  isRecoveringPassword = false;
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  recoverPassword(): void {
+    this.authService.recoverPassword(this.form.value.email).subscribe(
+      () => {
+        this.snackBar.open('Password recovery email sent', 'OK', {
+          duration: 5000,
+        });
+      },
+      (error) => {
+        this.snackBar.open(error.message, 'OK', { duration: 5000 });
+      }
+    );
+  }
+}
