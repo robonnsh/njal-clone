bee21685e0ed57c7d80a37114411388d088cda74
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-03 01:34:48 +0400

    small changes in header

--- src/app/shell/header/header.module.ts
+++ src/app/shell/header/header.module.ts
@@ -4,11 +4,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { HeaderComponent } from './header.component';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule, MatIconModule, MatToolbarModule, MatSidenavModule],
+  declarations: [HeaderComponent, ShoppingCartComponent],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
