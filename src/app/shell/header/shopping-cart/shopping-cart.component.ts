8fc9bdf8bd5e57c28ebf45a81b0297821cb5f733
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-03 01:35:06 +0400

    add shopping cart sidebar

---
+++ src/app/shell/header/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarToggleService } from '../../../services/navba-toggle/navbar-toggle.service';
+
+@Component({
+  selector: 'app-shopping-cart',
+  templateUrl: './shopping-cart.component.html',
+  styleUrl: './shopping-cart.component.scss',
+})
+export class ShoppingCartComponent {
+  constructor(private navbarToggleService: NavbarToggleService) {}
+  showLists(): boolean {
+    return this.navbarToggleService.showLists;
+  }
+}
