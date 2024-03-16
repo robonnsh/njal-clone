06001a4cead9bdf4ffdd37af53537c9acbe457ef
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-17 02:51:15 +0400

    change service for dummy data

--- src/app/services/home-products/home-products.service.ts
+++ src/app/services/home-products/home-products.service.ts
@@ -1,16 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Product } from '../../interfaces/product';
+import { Iproduct } from '../../interfaces/Iproduct.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HomeProductsService {
-  baseApiUrl: string = 'http://localhost:5016';
+  // baseApiUrl: string = 'http://localhost:5016';
+  // constructor(private http: HttpClient) {}
+
+  // getAllProduct(): Observable<Product[]> {
+  //   return this.http.get<Product[]>(this.baseApiUrl + '/api/Product');
+  // }
   constructor(private http: HttpClient) {}
 
-  getAllProduct(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseApiUrl + '/api/Product');
+  getAllProperties(): Observable<Iproduct[]> {
+    return this.http.get<any[]>('data/properties.json').pipe(
+      map((data) => {
+        const propertiesArray: Iproduct[] = [];
+        for (const id in data) {
+          if (data.hasOwnProperty(id)) {
+            propertiesArray.push(data[id]);
+          }
+        }
+        return propertiesArray;
+      })
+    );
   }
 }
