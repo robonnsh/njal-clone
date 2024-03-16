9e843fe04f5442929a70b7fee27a728bcb513ad5
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-17 02:50:56 +0400

    add new interface for product

---
+++ src/app/interfaces/Iproduct.interface.ts
@@ -0,0 +1,10 @@
+export interface Iproduct {
+  Id: number;
+  DesignerName: string;
+  ProductName: string;
+  Price: number;
+  Country: string;
+  Size: string;
+  Colour: string;
+  Description: string;
+}
