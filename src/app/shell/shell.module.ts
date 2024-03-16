15b821094b1fca4abb474ab8620da2bc96234c55
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-17 02:49:48 +0400

    organize and add admin module

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainComponent } from './main/main.component';
-import { ShellComponent } from './shell.component';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginModule } from './login/login.module';
-import { HttpClientModule } from '@angular/common/http';
 import { HeaderModule } from './header/header.module';
+import { LoginModule } from './login/login.module';
+import { AdminModule } from './admin-panel/admin.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ShellComponent } from './shell.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { ProductDetailsComponent } from './product-details/product-details.component';
 @NgModule({
-  declarations: [ShellComponent, MainComponent, HomeComponent, ProductCardComponent, FooterComponent],
+  declarations: [
+    ShellComponent,
+    MainComponent,
+    HomeComponent,
+    ProductCardComponent,
+    FooterComponent,
+    ProductDetailsComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,
     HttpClientModule,
     LoginModule,
     HeaderModule,
+    AdminModule,
   ],
   exports: [ShellComponent],
 })
