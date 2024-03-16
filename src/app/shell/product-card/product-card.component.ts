df6b10d42a7d75306eb0fa15888191609734f4ac
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-17 02:53:30 +0400

    init

--- src/app/shell/product-card/product-card.component.ts
+++ src/app/shell/product-card/product-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeProductsService } from '../../services/home-products/home-products.service';
 import { Product } from '../../interfaces/product';
+import { Iproduct } from '../../interfaces/Iproduct.interface';
 
 @Component({
   selector: 'app-product-card',
@@ -9,20 +10,26 @@ import { Product } from '../../interfaces/product';
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent implements OnInit {
-  products: Product[] = [];
+  // products: Product[] = [];
+  products: Array<Iproduct> = [];
   constructor(
     private router: Router,
     private homeProductService: HomeProductsService
   ) {}
 
+  // ngOnInit(): void {
+  //   this.homeProductService.getAllProduct().subscribe({
+  //     next: (products) => {
+  //       this.products = products;
+  //     },
+  //     error: (response) => {
+  //       console.log(response);
+  //     },
+  //   });
+  // }
   ngOnInit(): void {
-    this.homeProductService.getAllProduct().subscribe({
-      next: (products) => {
-        this.products = products;
-      },
-      error: (response) => {
-        console.log(response);
-      },
+    this.homeProductService.getAllProperties().subscribe((data) => {
+      this.products = data;
     });
   }
 }
