31fe042716a068df3df6e4988db8e22b7b37f076
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-22 02:56:05 +0400

    fetch data for new in products

--- src/app/shell/home/home.component.ts
+++ src/app/shell/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeProductsService } from '../../services/home-products/home-products.service';
+import { IProductBase } from '../../interfaces/iproduct-base';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products!: IProductBase[];
 
+  constructor(private homeProductService: HomeProductsService) {}
+  ngOnInit(): void {
+    this.homeProductService.getLastAddedFourProduct().subscribe((data) => {
+      console.log(data);
+      this.products = data;
+    });
+  }
 }
