be15d125425bae71abcb08aac3efe441c3930245
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-22 02:56:54 +0400

    make changes to correctly show products and details based on prudct id

--- src/app/shell/product-card/product-card.component.ts
+++ src/app/shell/product-card/product-card.component.ts
@@ -1,34 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeProductsService } from '../../services/home-products/home-products.service';
 import { Product } from '../../interfaces/product';
 import { INewInProduct } from '../../interfaces/INewIn.interface';
-
+import { IProductBase } from '../../interfaces/iproduct-base';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent implements OnInit {
-  NewInproducts: INewInProduct[] = [];
-  // NewInproducts: Array<Iproduct> = [];
-  constructor(
-    private router: Router,
-    private homeProductService: HomeProductsService
-  ) {}
-  ngOnInit(): void {
-    this.homeProductService.getLastAddedFourProduct().subscribe((data) => {
-      this.NewInproducts = data;
-    });
-  }
-  // ngOnInit(): void {
-  //   this.homeProductService.getNewInProducts().subscribe({
-  //     next: (products) => {
-  //       this.NewInproducts = products;
-  //     },
-  //     error: (response) => {
-  //       console.log(response);
-  //     },
-  //   });
-  // }
+  @Input() products!: Product[];
+  constructor() {}
+  ngOnInit(): void {}
 }
--- src/app/shell/product-card/product-card.module.ts
+++ src/app/shell/product-card/product-card.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductCardComponent } from './product-card.component';
 import { ProductDetailsComponent } from '../product-details/product-details.component';
+import { SlugifyPipe } from '../../Pipes/slugify.pipe';
+
 const routes: Routes = [
   {
-    path: 'product-details/:NewInProductId',
+    path: 'products/:id',
     component: ProductDetailsComponent,
   },
 ];
 
 @NgModule({
-  declarations: [ProductCardComponent, ProductDetailsComponent],
+  declarations: [ProductCardComponent, ProductDetailsComponent, SlugifyPipe],
   imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [ProductCardComponent],
 })
