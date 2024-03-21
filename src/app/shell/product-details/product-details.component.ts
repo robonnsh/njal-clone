a400b380fb2a07cb4483e81db16f3c0e1619ebc7
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-22 02:51:20 +0400

    make changes to get data from api for product details

--- src/app/shell/product-details/product-details.component.ts
+++ src/app/shell/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { INewInProduct } from '../../interfaces/INewIn.interface';
 import { HomeProductsService } from '../../services/home-products/home-products.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-product-details',
@@ -9,18 +10,20 @@ import { HomeProductsService } from '../../services/home-products/home-products.
   styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent implements OnInit {
-  NewInProduct!: INewInProduct;
+  productData: undefined | Product;
   constructor(
     private route: ActivatedRoute,
     private homeProductService: HomeProductsService
   ) {}
   ngOnInit(): void {
-    let NewInProductId = this.route.snapshot.paramMap.get('NewInProductId');
-    NewInProductId &&
+    const productId = this.route.snapshot.paramMap.get('id');
+    console.warn(productId);
+    productId &&
       this.homeProductService
-        .getNewInProducts(NewInProductId)
-        .subscribe((res) => {
-          this.NewInProduct = res;
+        .getProduct(+productId)
+        .subscribe((res: Product) => {
+          console.warn(res);
+          this.productData = res;
         });
   }
 }
