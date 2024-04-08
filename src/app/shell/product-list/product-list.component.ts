95864cda9985409dd19a8c93348cfa5e7187c393
robo ninoshvili <nnshtofficial@gmail.com>
2024-04-09 00:34:49 +0400

    init

--- src/app/shell/product-list/product-list.component.ts
+++ src/app/shell/product-list/product-list.component.ts
@@ -4,15 +4,23 @@ import { ActivatedRoute } from '@angular/router';
 import { HomeProductsService } from '../../services/home-products/home-products.service';
 import { IProductBase } from '../../interfaces/iproduct-base';
 import { StaticDataService } from '../../services/staticDataService/static-data.service';
+import { Iproduct } from '../../interfaces/iproduct';
+import { ItextCollection } from '../../interfaces/itext-collection';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent implements OnInit {
-  titles: any;
+  textCollection!: ItextCollection[];
+  titles: any[] = [];
   // gender!: number;
-  products: any;
+  products!: IProductBase[];
+  Name: string = '';
+  searchDesigner: string = '';
+  sortByParam: string = 'price';
+  sortDirection = 'asc';
   // Product: Product[] = [];
 
   constructor(
@@ -22,19 +30,91 @@ export class ProductListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // this.route.params.subscribe((params) => {
+    //   const genderId = +params['id'];
+    //   this.homeProductService.getAllProducts(genderId).subscribe({
+    //     next: (res) => {
+    //       this.products = res;
+    //     },
+    //     error: (error) => {
+    //       console.log(error);
+    //     },
+    //   });
+    // });
+    this.getProductByGender();
+    // static data
+    this.getTextCollection();
+    // this.getTitles();
+  }
+
+  getProductByGender() {
     this.route.params.subscribe((params) => {
       const genderId = +params['id'];
-      this.homeProductService.getProductByGender(genderId).subscribe((res) => {
-        this.products = res;
-        console.log(res);
-        console.log(this.route.snapshot.url.toString());
-      });
+      this.homeProductService
+        .getProductByGender(genderId)
+        .subscribe((res: Product[]) => {
+          if (typeof res === 'object' && res !== null) {
+            Object.keys(res).forEach((key: any) => {
+              console.log(`product: ${key}, Type: ${typeof res[key]}`);
+            });
+          }
+          console.log(res[0]);
+
+          this.products = res;
+          console.log(res);
+          console.log(this.route.snapshot.url.toString());
+          console.log(typeof res);
+        });
     });
+  }
 
-    // static data
+  onDesignerFilter() {
+    if (this.Name.trim()) {
+      this.searchDesigner = this.capitalizeFirstLetter(this.Name.trim());
+    } else {
+      this.searchDesigner = '';
+    }
+  }
 
-    this.staticData.getWomenTitlesEnd().subscribe((res) => {
-      this.titles = res;
+  onSortDirection() {
+    if (this.sortDirection === 'desc') {
+      this.sortDirection = 'asc';
+    } else {
+      this.sortDirection = 'desc';
+    }
+    console.log('clicked', this.sortDirection);
+  }
+  private capitalizeFirstLetter(value: string): string {
+    if (!value) return value;
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+  }
+
+  // Titles
+  // getTitles() {
+  //   this.route.params.subscribe((params) => {
+  //     const genderId = +params['id'];
+  //     this.staticData.getTextCollection(genderId).subscribe((res) => {
+  //       this.titles = res;
+  //     });
+  //   });
+  // }
+  // End paragraphs
+  // getTextCollection() {
+  //   this.route.params.subscribe((params) => {
+  //     const genderId = +params['id'];
+  //     this.staticData.getTextCollection(genderId).subscribe((res) => {
+  //       console.log(res);
+  //     });
+  //   });
+  // }
+
+  getTextCollection() {
+    this.route.params.subscribe((params) => {
+      const genderId = +params['id'];
+      this.staticData.getTextCollection(genderId).subscribe((res) => {
+        console.log(res);
+        this.textCollection = res;
+      });
     });
   }
 }
--- src/app/shell/product-list/product-list.module.ts
+++ src/app/shell/product-list/product-list.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-
+import { ProductListComponent } from './product-list.component';
+import { ProductCardModule } from '../product-card/product-card.module';
+import { SortPipe } from '../../Pipes/sort.pipe';
+import { FilterPipe } from '../../Pipes/filter.pipe';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductListComponent, SortPipe, FilterPipe],
+  imports: [CommonModule, FormsModule, ProductCardModule],
+  exports: [ProductListComponent],
 })
-export class ProductListModule { }
+export class ProductListModule {}
