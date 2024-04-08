31a2703a53ac992f747a5d890663293b44604140
robo ninoshvili <nnshtofficial@gmail.com>
2024-04-09 00:35:22 +0400

    add text collection interface for dummy data

---
+++ src/app/interfaces/itext-collection.ts
@@ -0,0 +1,9 @@
+export interface ItextCollection {
+  category: string;
+  womenTitleHeading?: string;
+  womenTitleParagraph?: string;
+  menTitleHeading?: string;
+  menTitleParagraph?: string;
+  heading: string;
+  paragraph: string;
+}
